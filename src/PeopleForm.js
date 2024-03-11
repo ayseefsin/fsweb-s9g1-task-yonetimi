43c25b16744e707ea48074012d67bbff68147fee
ayseefsin <ayse.efsin@yandex.com>
2024-03-11 23:53:33 +0300

    people form - rhf

--- src/PeopleForm.js
+++ src/PeopleForm.js
@@ -1,29 +1,40 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const PeopleForm = ({ kisiler, submitFn }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid, errors },
+  } = useForm({
+    mode: "onChange",
+    defaultValues: { title: "" },
+  });
   const [isim, setIsim] = useState("");
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    if (kisiler.includes(isim)) {
-      setError("Bu isim daha önce eklenmiş")
-    } else {
-      setError(null)
-    }
-  }, [isim, kisiler])
+  // useEffect(() => {
+  //   if (kisiler.includes(isim)) {
+  //     setError("Bu isim daha önce eklenmiş");
+  //   } else {
+  //     setError(null);
+  //   }
+  // }, [isim, kisiler]);
 
-  function handleIsimChange(e) {
-    setIsim(e.target.value);
-  }
+  // function handleIsimChange(e) {
+  //   setIsim(e.target.value);
+  // }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    submitFn(isim);
-    setIsim("");
+  function myHandleSubmit(d) {
+    // e.preventDefault();
+    submitFn(d.title);
+    // setIsim("");
+    reset();
   }
 
   return (
-    <form className="taskForm" onSubmit={handleSubmit}>
+    <form className="taskForm" onSubmit={handleSubmit(myHandleSubmit)}>
       <div className="form-line">
         <label className="input-label" htmlFor="title">
           İsim
@@ -31,20 +42,25 @@ const PeopleForm = ({ kisiler, submitFn }) => {
         <input
           className="input-text"
           id="title"
-          name="title"
+          // name="title"
+          {...register("title", {
+            required: "isim yazılmalıdır.",
+            minLength: {
+              value: 3,
+              message: "en az 3 karakter girilmelidir.",
+            },
+            validate: (value) =>
+              !kisiler.includes(value) || "bu isim daha önce eklendi.",
+          })}
           type="text"
-          onChange={handleIsimChange}
-          value={isim}
+          // onChange={handleIsimChange}
+          // value={isim}
         />
-        <p className="input-error">{error}</p>
+        <p className="input-error">{errors.title?.message}</p>
       </div>
 
       <div className="form-line">
-        <button
-          className="submit-button"
-          type="submit"
-          disabled={isim.length === 0 || error}
-        >
+        <button className="submit-button" type="submit" disabled={!isValid}>
           Ekle
         </button>
       </div>
